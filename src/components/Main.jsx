93a118fd4b74def400e1f0045afde4a9f3608aef
Bzakrzewski <30876128+Bzakrzewski@users.noreply.github.com>
2019-08-12 19:43:29 +0200

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,68 +1,18 @@
 import React, {Component} from 'react';
 import {browserHistory} from 'react-router';
-import styled from 'styled-components'
 import Coverflow from 'react-coverflow';
 import '../style.css';
+import GoogleMapReact from 'google-map-react';
 
 
-const Header = styled.div`
-  position: fixed;
-  display: inline-block;
-  z-index: 2;
-`
 
-const Header_backgorund = styled.div`
-  width: 98.3vw;
-  height: 15vh;
-  z-index: 2;
-`
 
-const Icon = styled.img`
-  height: 20vh;
-  weight: 10vw;
-  float: left;
-  margin-top: -2%;
-`
+const AnyReactComponent = () => <img src=
+      {require('../images/icons8-marker-48.png')}
+       alt=''
+        />;
 
-const Button = styled.button`
-  float: left;
-  width: 5%;
-  height: 25%;
-  font-size: 13px;
-  font-family: "Arial Rounded MT Bold", "Helvetica Rounded", Arial, sans-serif;
-  font-weight: 700;
-  margin: 1%;
-  margin-top: 2%;
-  background-color: Transparent;
-  background-repeat:no-repeat;
-  border: none;
-  cursor:pointer;
-  overflow: hidden;
-  outline:none;
-`
 
-const Background_photo = styled.img`
-  width: 100vw;
-  height: 102vh;
-  position: absolute;
-`
-
-const Apartament = styled.img`
-  width: 14vw;
-  height: 26vh;
-  margin: 5%;
-  margin-left: 9%;
-  margin-right: 9%;
-  margin-bottom: 0%;
-  z-index: 3;
-  border-radius: 25px;
-  border-style: solid;
-  border-width: 3px;
-  /*https://www.cssmatic.com/box-shadow*/
-  -webkit-box-shadow: -7px -5px 19px 0px rgba(0,0,0,0.75);
-  -moz-box-shadow: -7px -5px 19px 0px rgba(0,0,0,0.75);
-  box-shadow: -7px -5px 19px 0px rgba(0,0,0,0.75);
-`
 
 let pics = document.getElementsByClassName('coverflow__figure__3bk_C');
 
@@ -82,7 +32,7 @@ class Main extends Component {
   }
 
   componentDidMount = () => {
-    let coverFlowID = document.getElementsByClassName('coverFlowApartament');
+    let coverFlowID = document.getElementsByClassName('coverFlowApartment');
     let apartMainID = document.getElementsByClassName('apartmentsMain');
 
     for(let i=0; i < coverFlowID.length; i++ ){
@@ -103,30 +53,40 @@ class Main extends Component {
     }
   }
 
+  static defaultProps = {
+      center: {
+        lat: 51.121620,
+        lng: 17.032000
+      },
+      zoom: 15
+    };
+
   render() {
     return(
       <div>
-        <Header>
-          <Header_backgorund>
-            <Icon src={require('../images/my_logo.png')} alt='' />
-            <Button
+        <div className="header">
+          <div className="header_backgorund">
+            <img className="icon" src={require('../images/my_logo.png')} alt='' />
+            <button className="menu_btn"
               onClick={() => this.about()}
               onMouseEnter={(event) => this.mouseEnter(event)} onMouseLeave={(event) => this.mouseLeave(event)}
-            > About </Button>
-            <Button
+            > About </button>
+            <button className="menu_btn"
               onClick={() => this.offer()}
               onMouseEnter={(event) => this.mouseEnter(event)} onMouseLeave={(event) => this.mouseLeave(event)}
-            > Offer </Button>
-            <Button
+            > Offer </button>
+            <button className="menu_btn"
               onClick={() => this.contact()}
               onMouseEnter={(event) => this.mouseEnter(event)} onMouseLeave={(event) => this.mouseLeave(event)}
-            > Contact </Button>
-          </Header_backgorund>
-        </Header>
+            > Contact </button>
+          </div>
+        </div>
 
         <div className="zxc">
-          <Background_photo src={require('../images/backgorund_photo.jpg')}
-                 alt='' />
+          <img className="background_photo" src={require('../images/backgorund_photo.jpg')}
+                 alt='' style={{position: 'absolute',
+                                height: '102vh'
+               }}/>
           <Coverflow
             width="800" height="501"
             displayQuantityOfSide={2}
@@ -142,17 +102,17 @@ class Main extends Component {
               <img
                 src={require('../images/apartment0.jpg')}
                 alt=''
-                className='coverFlowApartament'
+                className='coverFlowApartment'
                 style={{
                   display: 'block',
                   width: '100%',
                 }}
               />
             </div>
-              <img src={require('../images/apartment1.jpg')} alt='' className='coverFlowApartament'/>
-              <img src={require('../images/apartment2.jpg')} alt='' className='coverFlowApartament'/>
-              <img src={require('../images/apartment3.jpg')} alt='' className='coverFlowApartament'/>
-              <img src={require('../images/apartment4.jpg')} alt='' className='coverFlowApartament'/>
+              <img src={require('../images/apartment1.jpg')} alt='' className='coverFlowApartment'/>
+              <img src={require('../images/apartment2.jpg')} alt='' className='coverFlowApartment'/>
+              <img src={require('../images/apartment3.jpg')} alt='' className='coverFlowApartment'/>
+              <img src={require('../images/apartment4.jpg')} alt='' className='coverFlowApartment'/>
           </Coverflow>
         </div>
         <div className="greyBackground center2"
@@ -161,13 +121,13 @@ class Main extends Component {
         <div className="apartments ">
 
 
-            <div className="apartmentsMain center2">
+            <div className="apartmentsMain center2 apartment_border_0">
               <div>
                 <p className="infos middle">Lorem ipsum</p>
-                <Apartament
+                <img
                   src={require('../images/apartment0.jpg')}
                   alt=''
-                  className="showed"
+                  className="showed apartment_style"
                 />
                 <p className="infos">Address</p>
                 <br />
@@ -182,16 +142,27 @@ class Main extends Component {
                 <p className="features">Monitoring</p>
                 </div>
                 <button className="showInvest"> Show investment </button>
+                <div style={{ height: '170px', width: '100%' }}>
+                  <GoogleMapReact
+                    bootstrapURLKeys={{ key: /*here is a key */ }}
+                    defaultCenter={this.props.center}
+                    defaultZoom={this.props.zoom} >
+                    <AnyReactComponent
+                      lat={51.122890}
+                      lng={17.031890}
+                      value='text' />
+                  </GoogleMapReact>
+                </div>
             </div>
 
 
-            <div className="apartmentsMain center2">
+            <div className="apartmentsMain center2 apartment_border_1">
               <div>
                 <p className="infos middle">Lorem ipsum</p>
-                <Apartament
+                <img
                   src={require('../images/apartment1.jpg')}
                   alt=''
-                  className="showed"
+                  className="showed apartment_style"
                 />
                 <p className="infos">Address</p>
                 <br />
@@ -209,13 +180,13 @@ class Main extends Component {
             </div>
 
 
-            <div className="apartmentsMain center2">
+            <div className="apartmentsMain center2 apartment_border_2">
               <div>
                 <p className="infos middle">Lorem ipsum</p>
-                <Apartament
+                <img
                   src={require('../images/apartment2.jpg')}
                   alt=''
-                  className="showed"
+                  className="showed apartment_style"
                 />
                 <p className="infos">Address</p>
                 <br />
@@ -233,13 +204,13 @@ class Main extends Component {
             </div>
 
 
-            <div className="apartmentsMain center2">
+            <div className="apartmentsMain center2 apartment_border_3">
               <div>
                 <p className="infos middle">Lorem ipsum</p>
-                <Apartament
+                <img
                   src={require('../images/apartment3.jpg')}
                   alt=''
-                  className="showed"
+                  className="showed apartment_style"
                 />
                 <p className="infos">Address</p>
                 <br />
@@ -257,13 +228,13 @@ class Main extends Component {
             </div>
 
 
-            <div className="apartmentsMain center2">
+            <div className="apartmentsMain center2 apartment_border_4">
               <div>
                 <p className="infos middle">Lorem ipsum</p>
-                <Apartament
+                <img
                   src={require('../images/apartment4.jpg')}
                   alt=''
-                  className="showed"
+                  className="showed apartment_style"
                 />
                 <p className="infos">Address</p>
                 <br />
